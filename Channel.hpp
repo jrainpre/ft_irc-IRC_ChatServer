6ee51ed2526228ced095ee06caefc03999ba9073
jrainpre <jonathan.rainprechter+42@gmail.com>
2023-06-27 10:05:08 +0200

    feat: added first draw of Channel and Client Class

---
+++ Channel.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Client.hpp"
+
+class Channel {
+private:
+	std::string _name;
+	std::string _topic;
+	std::vector<Client> _users;
+public:
+	Channel(std::string name);
+	~Channel();
+	std::string getName();
+	std::string getTopic();
+	std::vector<Client> getUsers();
+	void setName(std::string name);
+	void setTopic(std::string topic);
+	void addUser(Client user);
+	void removeUser(Client user);
+	bool check_channel_name(std::string name);
+};
