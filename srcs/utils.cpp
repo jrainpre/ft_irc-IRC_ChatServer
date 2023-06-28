8c167575a43d764857dfdd5b37da8195212c88f3
Jakob Klocker <jakobklocker@hotmail.com>
2023-06-28 18:04:15 +0200

    feat: splitting client commands

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -19,3 +19,25 @@ bool    validPort(std::string port)
         return 0;
     return 1;
 }
+
+std::vector<std::string> split(std::string &s, std::string delimit)
+{
+    size_t pos_start = 0;
+    size_t pos_end;
+    size_t len_delimit = delimit.size();
+    std::string substr;
+    std::vector<std::string> ret;
+
+    while((pos_end = s.find(delimit, pos_start)) != std::string::npos)
+    {
+        substr = s.substr(pos_start, pos_end);
+        ret.push_back(substr);
+        s.erase(pos_start, pos_end + len_delimit);
+    }
+    if(!s.empty())
+    {
+        ret.push_back(s);
+        s.clear();
+    }
+    return ret;
+}
