40f07e537ee55c4fe62ce11d40e3e4e778589634
jrainpre <jonathan.rainprechter+42@gmail.com>
2023-07-11 13:43:13 +0200

    feature: get whole cmd out of splited strung

--- srcs/Privmsg.cpp
+++ srcs/Privmsg.cpp
@@ -2,10 +2,36 @@
 
 typedef void (*CommandFunction)(Server& server, Client& client, std::vector<std::string>  &cmd);
 
+std::string getWholeCmd(std::vector<std::string> &cmd)
+{
+	std::string wholeCmd;
+	for (size_t i = 0; i < cmd.size(); i++)
+	{
+		wholeCmd += cmd[i];
+		if (i != cmd.size() -1)
+			wholeCmd += " ";
+	}
+}
+
 void cmdPrivmsg(Server& server, Client& client, std::vector<std::string>  &cmd)
 {
 	//parse message  form client 
+	try
+	{
+		std::string recipient = cmd[1];
+
+		
+	}
+	catch(const std::exception& e){}
+	
+
 	//PRIVMSG recipient :Hello, how are you?
+PRIVMSG
+recipient
+:Hello,
+how
+are
+you?
 	//check if recipient is a channel
 	//if yes --> send message to channel
 	//if no --> check if recipient is a valid nickname of an online user
